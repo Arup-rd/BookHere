f4ba6712e01dfdad85c395742b653de5a17ce2f5
Misbauddin Chowdhury Shahan <shahan953@hotmail.com>
2019-04-26 19:58:13 +0600

    all

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  DB_URI: 'mongodb://test:01738arup@ds233596.mlab.com:33596/book-here',
+  SECRET: 'SecretKey'
+  // AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  // AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  // STRIPE_SK: process.env.STRIPE_SK
+}
